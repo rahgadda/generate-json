aafecea1f70ea20d40e9970f1f1c3cec302e0aa5
rahgadda <gaddam.rahul@gmail.com>
2021-03-29 09:29:34 +0000

    Initial Draft

--- action/index.js
+++ action/index.js
@@ -6,19 +6,24 @@ const destinationFolderLocation = "../response"
 
 //Read files in books folder
 fs.readdirSync(sourceFolderLocation).forEach(fileName => {
+
     if (fileName.toString().endsWith(".json")) {
-        console.log("Processing File - " + fileName);
-        processTxtFile(fileName);
-        console.log("Completed Processing File - " + fileName);
+        try {
+            console.log("Processing File - " + fileName);
+            processJsonFile(fileName);
+            console.log("Completed Processing File - " + fileName);
+        } catch (err) {
+            console.log("Error Processing File - " + fileName);
+        }
     }
 });
 
 //Processing each file
-function processTxtFile(fileName) {
-    fs.readFile(sourceFolderLocation+"/"+fileName, 'utf8', function (err,data) {
+function processJsonFile(fileName) {
+    fs.readFile(sourceFolderLocation + "/" + fileName, 'utf8', function (err, data) {
         if (err) {
             return console.log("Error processing " + fileName + " " + err);
         }
-        console.log("Processing JSON file"+ fileName + " with data" +data);
+        console.log("Processing JSON file" + fileName + " with data" + data);
     });
 }
