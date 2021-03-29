6d1a85dba5465e42205f92eae070023d1bb8c432
rahgadda <gaddam.rahul@gmail.com>
2021-03-29 09:26:09 +0000

    Initial Draft

---
+++ action/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const dummyjson = require('dummy-json');
+
+const sourceFolderLocation = "../data"
+const destinationFolderLocation = "../response"
+
+//Read files in books folder
+fs.readdirSync(sourceFolderLocation).forEach(fileName => {
+    if (fileName.toString().endsWith(".json")) {
+        console.log("Processing File - " + fileName);
+        processTxtFile(fileName);
+        console.log("Completed Processing File - " + fileName);
+    }
+});
+
+//Processing each file
+function processTxtFile(fileName) {
+    fs.readFile(sourceFolderLocation+fileName, 'utf8', function (err,data) {
+        if (err) {
+            return console.log("Error processing " + fileName + " " + err);
+        }
+        console.log("Processing JSON file"+ fileName + " with data" +data);
+    });
+}
