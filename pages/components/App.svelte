5f52f85b0ef31571e58a4d7161ed4a336f93059f
rahgadda <gaddam.rahul@gmail.com>
2021-03-29 16:35:15 +0000

    Initial Draft

--- pages/components/App.svelte
+++ pages/components/App.svelte
@@ -1,5 +1,36 @@
 <script>
-	let name = 'world';
+  import Row from "svelte-atoms/Grids/Row.svelte";
+  import Cell from "svelte-atoms/Grids/Cell.svelte";
+  import Block from "svelte-atoms/Block.svelte";
+  import DropZone from "svelte-atoms/DropZone.svelte";
+
+  let fileName = "";
+  const onChange = e => {
+    const file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
+    fileName = file ? file.name : "";
+  };
 </script>
 
-<h1>Hello world!</h1>
+<Row>
+  <Cell><Block /></Cell>
+</Row>
+<Row>
+  <Cell><Block /></Cell>
+</Row>
+<Row>
+  <Cell><Block /></Cell>
+</Row>
+<Row>
+  <Cell><Block /></Cell>
+</Row>
+<Row>
+  <Cell xs={3}>
+  </Cell>
+  <Cell xs={6}>
+    <Block>
+        <DropZone fileTitle={fileName} dropOnPage on:drop={onChange} on:change={onChange} />
+    </Block>
+  </Cell>
+  <Cell xs={3}>
+  </Cell>
+</Row>
--- pages/rollup.config.js
+++ pages/rollup.config.js
@@ -5,6 +5,7 @@ import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
+import styles from "rollup-plugin-styles";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -17,6 +18,7 @@ export default {
     file: "public/bundle.js"
   },
   plugins: [
+    styles(),  
     svelte({
       // enable run-time checks when not in production
       dev: !production,
