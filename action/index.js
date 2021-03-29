0fbbcb939796d0795eda813c62f1ace18bc4dc4f
rahgadda <gaddam.rahul@gmail.com>
2021-03-29 09:37:24 +0000

    Initial Draft

--- action/index.js
+++ action/index.js
@@ -7,7 +7,7 @@ const destinationFolderLocation = "../response"
 //Read files in books folder
 fs.readdirSync(sourceFolderLocation).forEach(fileName => {
 
-    if (fileName.toString().endsWith(".json")) {
+    if (fileName.toString().endsWith(".hbs")) {
         try {
             console.log("Processing File - " + fileName);
             processJsonFile(fileName);
@@ -25,5 +25,15 @@ function processJsonFile(fileName) {
             return console.log("Error processing " + fileName + " " + err);
         }
         console.log("Processing JSON file" + fileName + " with data" + data);
+        const jsonData = dummyjson.parse(data);
+        console.log("Generated JSON " + jsonData);
+        hbs2Json(jsonData,fileName)
     });
+}
+
+// Write Json file
+function hbs2Json(jsonData, fileName) {
+    fs.writeFileSync(
+        destinationFolderLocation + "/" + fileName.toString().replace('.hbs','.json'), 
+        JSON.stringify(jsonData));
 }
