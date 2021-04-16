589463bb27e34e8e59b0c16bf1c48ec29d0182ed
rahgadda <gaddam.rahul@gmail.com>
2021-04-16 10:33:16 +0000

    Initial Draft

--- pages/components/JsonGenerator.svelte
+++ pages/components/JsonGenerator.svelte
@@ -1,30 +1,60 @@
+<script>
+    import { onMount } from "svelte";
+    const apiURL =
+        "https://raw.githubusercontent.com/rahgadda/generate-json/main/";
+    $: inputTemplate = "";
+    $: jsonOutput = "";
+
+    onMount(async function () {
+        let response = await fetch(apiURL + "data/sample.hbs");
+        inputTemplate = await response.text();
+        response = await fetch(apiURL + "response/sample.json");
+        jsonOutput = await response.text();
+    });
+</script>
+
+<main class="container">
+    <header class="header">
+        <h1 class="header-title">JSON Generator</h1>
+    </header>
+    <button class="button">Genearte Json</button><br />
+    <div class="html-editor">
+        <div class="left-panel">
+            <textarea bind:value={inputTemplate} class="source" />
+        </div>
+        <div class="right-panel">
+            <pre class="output">{jsonOutput}</pre>
+        </div>
+    </div>
+</main>
+
 <style>
-	.container{
+    .container {
         background: #ff3e00d6;
-        padding:10px 30px;
+        padding: 10px 30px;
     }
     .header {
         height: 10vh;
         display: flex;
         align-items: center;
         justify-content: center;
-
     }
     .header-title {
         margin: 0;
-        color:#fff;
+        color: #fff;
     }
-    .button{
+    .button {
         display: flex;
-        align-items:flex-end;
+        align-items: flex-end;
     }
     .html-editor {
         width: 100%;
         display: flex;
-        align-items:flex-start;
+        align-items: flex-start;
         justify-content: space-evenly;
     }
-    .left-panel, .right-panel {
+    .left-panel,
+    .right-panel {
         width: 50%;
         border: solid 1px black;
         height: 85vh;
@@ -48,32 +78,3 @@
         padding: 0 2em;
     }
 </style>
-
-<script>
-    import { onMount } from 'svelte';
-    const apiURL = "https://raw.githubusercontent.com/rahgadda/generate-json/main/";
-	$: inputTemplate= "";
-    $: jsonOutput= "";
-
-    onMount(async function() {
-        let response = await fetch(apiURL+"data/sample.hbs");
-        inputTemplate = await response.text();
-        response = await fetch(apiURL+"response/sample.json");
-        jsonOutput = await response.text();
-    });
-</script>
-
-<main class="container">
-	<header class="header">
-			<h1 class="header-title">JSON Generator</h1>
-	</header>
-    <button class="button">Genearte Json</button><br/>
-	<div class="html-editor">
-			<div class="left-panel">
-					<textarea bind:value={inputTemplate} class="source"></textarea>
-			</div>
-			<div class="right-panel">
-					<pre class="output">{jsonOutput}</pre>
-			</div>
-	</div>
-</main>
