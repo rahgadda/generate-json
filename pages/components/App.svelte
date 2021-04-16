1b1f9278895c6d5c3e0645e119a6e815f9648ebf
rahgadda <gaddam.rahul@gmail.com>
2021-04-16 08:29:32 +0000

    Initial Draft

--- pages/components/App.svelte
+++ pages/components/App.svelte
@@ -1,6 +1,13 @@
 <script>
 	import JsonGenerator from './JsonGenerator.svelte';
     import Login from './Login.svelte';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        const parmas= new URLSearchParams(window.location.search)
+        console.log("Loaded with code = "+parmas.get("code"));
+    });
+    
 </script>
 
 <Login />
