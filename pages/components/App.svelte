50db42c0a6fa29e2c967aa7dc024294a77b26449
rahgadda <gaddam.rahul@gmail.com>
2021-04-16 10:01:56 +0000

    Initial Draft

--- pages/components/App.svelte
+++ pages/components/App.svelte
@@ -4,12 +4,12 @@
     import Login from "./Login.svelte";
 
     const parmas = new URLSearchParams(window.location.search);
-    let urlToken = parmas.get("code");
+    const urlToken = parmas.get("code");
     console.log(urlToken);
 </script>
 
-<h1>Toke is {urlToken} </h1>
-
-{#if {urlToken} }
-    <h1>Toke is {urlToken} </h1>
+{#if urlToken}
+    <JsonGenerator />
+{:else}
+    <Login />
 {/if}
