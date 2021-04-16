979cb252c2d0bfb3050803b8b993e37326c7f3df
rahgadda <gaddam.rahul@gmail.com>
2021-04-16 08:45:38 +0000

    Initial Draft

--- pages/components/App.svelte
+++ pages/components/App.svelte
@@ -1,22 +1,21 @@
 <script>
-	import JsonGenerator from './JsonGenerator.svelte';
-    import Login from './Login.svelte';
-    import { onMount } from 'svelte';
+    import JsonGenerator from "./JsonGenerator.svelte";
+    import Login from "./Login.svelte";
+    import { onMount } from "svelte";
 
-    let urlToken=null;
+    let urlToken = null;
 
     onMount(() => {
-        const parmas= new URLSearchParams(window.location.search)
+        const parmas = new URLSearchParams(window.location.search);
         urlToken = parmas.get("code");
-        if(urlToken){
-            console.log("Loaded with code = "+urlToken);
+        if (urlToken) {
+            console.log("Loaded with code = " + urlToken);
         }
     });
-    
 </script>
 
-{#if {urlToken}}
-    <Login />
-{:else}
+{#if { urlToken }}
     <JsonGenerator />
+{:else}
+    <Login />
 {/if}
